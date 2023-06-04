87aa83be0366cbfd18ce3d8b4d0e59b41adfe9a6
Termux-nihan <135256106+Termux-nihan@users.noreply.github.com>
2023-06-04 23:27:32 +0600

    Add files via upload

--- New-tool.py
+++ New-tool.py
@@ -13,7 +13,6 @@ from bs4 import BeautifulSoup as sop
 from concurrent.futures import ThreadPoolExecutor as ThreadPool
 import os
 import random
-import requests,bs4,json,sys,random,datetime,time,re,subprocess,platform,struct
 from bs4 import BeautifulSoup as sop
 from concurrent.futures import ThreadPoolExecutor as tred
 import base64
